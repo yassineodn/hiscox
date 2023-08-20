8b9e92f8b6d89b20c8ab3cea4cffaf80b8448e4c
yassineodn <132125233+yassineodn@users.noreply.github.com>
2023-08-20 19:23:37 +0200

    Add files via upload

---
+++ Run Hiscox Frontoffice.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Run Hiscox Frontoffice</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>199ecc8f-51b0-40c6-bd04-a220fe65211d</testSuiteGuid>
+   <testCaseLink>
+      <guid>57db061a-8e0d-4570-a020-4ef7fca2781d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Hiscox - Front Office Desktop</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
